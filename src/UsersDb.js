4a880a27bd5aafcfef76369160863dccf1b71aa9
unknown <ivo1.biocic1@gmail.com>
2021-12-13 23:28:48 +0100

    prva verzija imenika

---
+++ src/UsersDb.js
@@ -0,0 +1,40 @@
+export const usersDb = {
+  A: [
+    {id:10, ime: "Ante", prezime: "Askdjhf", tel: "+2736234"},
+    {id:20, ime: "Lidija", prezime: "Asksldkjfdjhf", tel: "+27234234663675"},
+    {id:30, ime: "Mate", prezime: "Aasashjjhf", tel: "+272342342345"},
+    {id:40, ime: "Mirna", prezime: "Asksldkjfdjhf", tel: "00255563675"}
+  ],
+  B: [
+    {id:11, ime: "Lucija", prezime: "Bnnnnnnnn", tel: "0435362763675"},
+    {id:22, ime: "Ivo", prezime: "Bzzzzzzzz", tel: "+27232323445663675"},
+    {id:33, ime: "Zdravko", prezime: "Blllllllll", tel: "+2799234234234975"},
+    {id:44, ime: "Nenad", prezime: "Bccccccccc", tel: "+25556367234234234234235"}
+  ],
+  C: [
+    {id:12, ime: "Ana", prezime: "Cnnnnnnnn", tel: "00234232323232763675"},
+    {id:23, ime: "Martina", prezime: "Czzzzzzzz", tel: "+2723456623233675"},
+    {id:34, ime: "Iva", prezime: "Clllllllll", tel: "+2799975323233"},
+    {id:45, ime: "Vito", prezime: "Cccccccccc", tel: "+25556367523"}
+  ],
+  D: [
+    {id:13, ime: "Stjepan", prezime: "Dnnnnnnnn", tel: "+27362763675"},
+    {id:24, ime: "Ivan", prezime: "Dzzzzzzzz", tel: "+272345663675"},
+    {id:35, ime: "Živko", prezime: "Dlllllllll", tel: "+2799975"},
+    {id:46, ime: "Robert", prezime: "Dccccccccc", tel: "+255563675"}
+  ],
+  E: [
+    {id:14, ime: "Stjepan", prezime: "Ennnnnnnn", tel: "+27362763675"},
+    {id:25, ime: "Ivan", prezime: "Ezzzzzzzz", tel: "+272345663675"},
+    {id:36, ime: "Živko", prezime: "Elllllllll", tel: "+2799975"},
+    {id:47, ime: "Robert", prezime: "Eccccccccc", tel: "+255563675"},
+    {id:58, ime: "Hrvoje", prezime: "Ecsdfccccc", tel: "+23423423409"}
+  ],
+  F: [
+    {id:15, ime: "Miro", prezime: "Fnnnnnnnn", tel: "0034763675"},
+    {id:26, ime: "Željko", prezime: "Fzzzzzzzz", tel: "+342345663675"},
+    {id:37, ime: "Kruno", prezime: "Flllllllll", tel: "+434799975"},
+    {id:48, ime: "Dražen", prezime: "Fccccccccc", tel: "+239847893675"},
+    {id:59, ime: "Sanja", prezime: "Fcsdfccccc", tel: "+2342389473409"}
+  ],
+};
